7469ef9f21afc91194be9bce3bd06e3a9b4fe059
Ryan Namgung <heon.namgung@richmond.edu>
2018-06-20 00:28:13 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,7 +2,7 @@ def reformat_languages(languages)
   # your code here
   new_hash = {}
   languages.each do |style, lan|
-    lang.each do |language, hash|
+    lan.each do |language, hash|
       if new_hash[language] == nil
         new_hash[language] = hash
         new_hash[language][:style] = [style]
