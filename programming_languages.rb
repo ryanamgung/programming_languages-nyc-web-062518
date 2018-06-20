ebd705ad1425a5a1233b09908708376f5bb91bca
Ryan Namgung <heon.namgung@richmond.edu>
2018-06-20 00:25:53 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,7 +4,8 @@ def reformat_languages(languages)
   languages.each do |style, lan|
     lang.each do |language, hash|
       if new_hash[language] == nil
-        new_hash[language] == hash
+        new_hash[language] = hash
+        new_hash[language][:style] = style
       end
     end
   end
